47b6733beb5a8d0961e092dba64f9ce9a0a5bd22
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2026-01-21 21:02:03 -0300

    feat: add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/" className="logo">
+          <Image
+            src="/icons/logo.png"
+            alt="Dev Event Logo"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <ul>
+          <Link href="/">Home</Link>
+          <Link href="/">Events</Link>
+          <Link href="/">Create Event</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
